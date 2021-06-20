6a3fe02916d105d086d4712c4ad59be49f01c6c6
Anthony <anth19800@hotmail.com>
2021-06-19 20:04:09 -0400

    [Add] Some prelim stuff

---
+++ Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ButtonFunctions : MonoBehaviour {
+    public void StartGame() {
+        Debug.Log("Loading game scene...");
+        //UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+    }
+    public void QuitGame(){
+        Application.Quit();
+    }
+
+    
+}
---
+++ Assets/Scripts/TriggerEnter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEnter : MonoBehaviour
+{
+    public UnityEvent withinRangeEvent;
+    public UnityEvent exitRangeEvent;
+
+    private void OnTriggerEnter(Collider other) {
+        withinRangeEvent?.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other) {
+        exitRangeEvent?.Invoke();
+    }
+}
